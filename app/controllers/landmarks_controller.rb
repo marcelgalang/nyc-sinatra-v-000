f2daada2ec1deb956d430400df399808729d52ff
Marcel Galang <marcelgalang@gmail.com>
2016-09-13 13:40:42 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,12 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmark/index'
+  end
+
+  get'/landmark/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/show'
+  end
 end
